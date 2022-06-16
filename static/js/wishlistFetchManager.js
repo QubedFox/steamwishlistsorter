814e6e3645d28c3a400a7e167231b6247dbdf34c
Nicholas Taplin <nicholas.taplin@gmail.com>
2022-06-17 08:49:48 +0930

    Change fetch request function

--- static/js/wishlistFetchManager.js
+++ static/js/wishlistFetchManager.js
@@ -3,10 +3,20 @@ function fetchWishlist(profileURL){
     const urlSuffix = 'wishlistdata/';
 
     let url = urlPrefix + profileURL.slice(27, profileURL.length) + urlSuffix;
+    
+    console.log("Sending request to:", url);
 
-    fetch(url, {mode:'no-cors'})
+    fetch(url, {
+        method:'GET',
+        mode:'cors',
+        credentials:'include',
+        headers: new Headers({
+            'Content-Type':'application/json',
+            'Origin':'https://qubedfox.github.io/steamwishlistsorter/',
+            'Access-Control-Allow-Origin':'https://qubedfox.github.io/steamwishlistsorter/'
+        })
+    })
         .then(response => JSON.stringify(response))
         .then(data => console.log(data))
-        .catch(error => console.log(error));
-      
+        .catch(error => console.log(error));      
 }
